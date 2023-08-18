274efcf3fc1faeb9172e7cb80ed339f9362bcc66
Sreekanth K B <sreekanth@applab.qa>
2023-08-18 11:00:54 +0530

    Initial Commit

---
+++ AvacadoSampleApp/AvacadoSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvacadoSampleAppApp.swift
+//  AvacadoSampleApp
+//
+//  Created by Sreekanth on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AvacadoSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
